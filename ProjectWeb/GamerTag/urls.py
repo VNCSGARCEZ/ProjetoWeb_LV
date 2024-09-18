8c5d884cce77232277f0a187ad9f8dc6f75ee338
vncsg <vncsgarcez@gmail.com>
2024-09-17 23:51:13 -0300

    excluir analise

--- ProjectWeb/GamerTag/urls.py
+++ ProjectWeb/GamerTag/urls.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import IndexView, HomeView, AnalyzeView
+from .views import IndexView, HomeView, AnalyzeView, delete_review
 
 urlpatterns = [
     path('', IndexView.as_view(), name='inicio'),
     path('home/', HomeView.as_view(), name='home'),
     path('analyze/', AnalyzeView.as_view(), name='analyze'),
+    path('delete_review/<int:review_id>/', delete_review, name='delete_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ProjectWeb/GamerTag/views.py
+++ ProjectWeb/GamerTag/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
+from PIL import Image  # Biblioteca para manipulação de imagens
 
 # View para a página de login
 class IndexView(TemplateView):
@@ -23,16 +24,26 @@ class HomeView(TemplateView):
         rating = request.POST.get('rating')
         game_image = request.FILES.get('gameImage')
 
+        # Verificação do tamanho da imagem
+        if game_image:
+            img = Image.open(game_image)
+            max_width, max_height = 600, 900
+            if img.width > max_width or img.height > max_height:
+                # Se a imagem exceder o tamanho permitido, retorna a página com uma mensagem de erro
+                return render(request, 'home.html', {
+                    'error_message': f'A imagem deve ter no máximo {max_width}x{max_height} pixels.',
+                })
+
         # Armazenar os dados na sessão
         if 'reviews' not in request.session:
             request.session['reviews'] = []
 
-        # Criar um dicionário para armazenar as informações da análise
+        # Criar um dicionario para armazenar as informações da análise
         review = {
             'game_name': game_name,
             'game_review': game_review,
             'rating': rating,
-            'game_image': request.FILES.get('gameImage')  # Armazenar o arquivo
+            'game_image': game_image.name if game_image else None  # Armazenar o nome do arquivo
         }
 
         request.session['reviews'].append(review)
@@ -48,3 +59,12 @@ class AnalyzeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['reviews'] = self.request.session.get('reviews', [])
         return context
+
+# Função para excluir uma análise com base no índice
+def delete_review(request, review_id):
+    if 'reviews' in request.session:
+        reviews = request.session['reviews']
+        if 0 <= review_id < len(reviews):
+            del reviews[review_id]
+            request.session.modified = True
+    return redirect('/analyze/')
